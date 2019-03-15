d7664164b3b63e04f793290546adba3ab2c1b5ff
Erik Copel Rothman <git.erikcr@gmail.com>
2019-03-15 19:04:24 -0300

    Add struct.c

---
+++ P1/struct.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct student{
+    char name[100];
+    int score;
+} student;
+
+int main(){
+    student joao;
+
+    joao.name = "joao";
+    joao.score = 7;
+
+}
+
